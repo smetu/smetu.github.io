339e0f07b5fbb6aec4a99022e1f6ca0a8d586947
Kaveh Kouvali <81914120+kaveh-dev@users.noreply.github.com>
2025-11-29 12:09:03 +0330

    Updated the allowedl list link

--- assets/js/constants.js
+++ assets/js/constants.js
@@ -2,7 +2,7 @@ const OWNER = "smetbz";
 const REPO = "smetbz.github.io";
 const PROXY_URL = `https://api.smtu.ir/github/`;
 const BRIDGE_URL = `https://api.smtu.ir/bridge/`;
-const ALLOWED_LIST = `https://cdn.jsdelivr.net/gh/smetbz/smetbz.github.io@main/allowedList.json`;
+const ALLOWED_LIST = `https://raw.githubusercontent.com/smetbz/smetbz.github.io/refs/heads/main/allowedList.json`;
 const PERSIAN_NAMES = `https://cdn.jsdelivr.net/gh/smetbz/smetbz.github.io@main/persianNames.json`;
 
 const categoriesInPersian = {
